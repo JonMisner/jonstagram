a77b093fb2044a3b2371be77e288be08bdee1ffe
Jon Misner <jonmisner86@gmail.com>
2021-06-27 15:03:27 -0500

    start of timeline

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -24,16 +24,65 @@ export async function getUserByUserId(userId) {
   return user;
 }
 
-export async function getSuggestedProfiles(userId) {
-  const result = await firebase
-  .firestore()
-  .collection("users")
-  .limit(10)
-  .get();
+export async function getSuggestedProfiles(userId, following) {
+   let query = firebase.firestore().collection('users');
+ 
+   if (following.length > 0) {
+     query = query.where('userId', 'not-in', [...following, userId]);
+   } else {
+     query = query.where('userId', '!=', userId);
+   }
+   const result = await query.limit(10).get();
+ 
+   const profiles = result.docs.map((user) => ({
+     ...user.data(),
+     docId: user.id
+   }));
+ 
+   return profiles;
+ }
 
-  return result.docs
-   .dotmap ((user) => ({ 
-      ...user.data(),
-   docID: user.id
-});)
-}
+ export async function updateLoggedInUserFollowing(
+   loggedInUserDocId, // currently logged in user document id (karl's profile)
+   profileId, // the user that karl requests to follow
+   isFollowingProfile // true/false (am i currently following this person?)
+ ) {
+   return firebase
+     .firestore()
+     .collection('users')
+     .doc(loggedInUserDocId)
+     .update({
+       following: isFollowingProfile
+         ? FieldValue.arrayRemove(profileId)
+         : FieldValue.arrayUnion(profileId)
+     });
+ }
+ 
+ export async function updateFollowedUserFollowers(
+   profileDocId, // currently logged in user document id (karl's profile)
+   loggedInUserDocId, // the user that karl requests to follow
+   isFollowingProfile // true/false (am i currently following this person?)
+ ) {
+   return firebase
+     .firestore()
+     .collection('users')
+     .doc(profileDocId)
+     .update({
+       followers: isFollowingProfile
+         ? FieldValue.arrayRemove(loggedInUserDocId)
+         : FieldValue.arrayUnion(loggedInUserDocId)
+     });
+ }
+
+ export async function getPhotos (userid, following) {
+    const result = await firebase
+    .firestore()
+    .collection('photos')
+    .where('userId', 'in', following)
+    .get();
+
+    const uerFollowedPhotos = result.doc.map((photo) => ({
+       ...photo.data(),
+       docId: photo.id
+    }));
+ }
